89a4be22905491edf0bc477a932f1b108232b594
David Joyner <djoyner@ucsb.edu>
2024-10-19 13:59:39 -0700

    Added the display of user email

--- app/screens/List.tsx
+++ app/screens/List.tsx
@@ -1,12 +1,23 @@
 import {View, Text, Button} from 'react-native'
-import React from 'react'
-import { FIREBASE_AUTH } from '../../FirebaseConfig';
+import React, { useEffect, useState } from 'react'
+import { FIREBASE_APP, FIREBASE_AUTH } from '../../FirebaseConfig';
+
+import { getAuth, onAuthStateChanged, User } from "firebase/auth";
 
 const List = ({navigation}: any) => {
+    const [user, setUser] = useState<User | null>(null);
+    useEffect(() =>{
+      onAuthStateChanged(FIREBASE_AUTH,(user)=>{
+        console.log('user',user);
+        setUser(user);
+      })
+    }, [])
+
     return(
         <View style={{flex: 1, justifyContent: 'center', alignItems: 'center'}}>
             <Button onPress={() => navigation.navigate('details')} title="Open Details" />
             <Button onPress={() => FIREBASE_AUTH.signOut()} title="Log out" />
+            <Text> {user?.email} </Text>
         </View>
     )
 }
